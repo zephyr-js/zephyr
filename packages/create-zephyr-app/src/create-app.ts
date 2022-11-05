112aa3986fa92246972c4ef7ed7df8b2be822bfd
kakeng <kakengloh@gmail.com>
2022-11-05 16:32:31 +0800

    Edit package.json info

--- packages/create-zephyr-app/src/create-app.ts
+++ packages/create-zephyr-app/src/create-app.ts
@@ -1,6 +1,12 @@
 import path from 'path';
 import chalk from 'chalk';
-import { isWriteable, mkdir, PackageManager } from '@/utils/common';
+import fs from 'fs';
+import {
+  isFolderEmpty,
+  isWriteable,
+  mkdir,
+  PackageManager,
+} from '@/utils/common';
 import { installTemplate } from './templates';
 import { gitInit } from './utils/git';
 
@@ -15,19 +21,24 @@ export const createApp = async ({
 }: CreateAppOptions): Promise<void> => {
   const root = path.resolve(appPath);
 
-  // if (!(await isWriteable(root))) {
-  //   console.error(
-  //     'The application path is not writable, please check folder permissions and try again.',
-  //   );
-  //   console.error(
-  //     'It is likely you do not have write permissions for this folder.',
-  //   );
-  //   process.exit(1);
-  // }
+  if (!(await isWriteable(root))) {
+    console.error(
+      'The application path is not writable, please check folder permissions and try again.',
+    );
+    console.error(
+      'It is likely you do not have write permissions for this folder.',
+    );
+    process.exit(1);
+  }
 
   const appName = path.basename(root);
 
   await mkdir(root);
+  if (!isFolderEmpty(root, appName)) {
+    process.exit(1);
+  }
+
+  const useYarn = packageManager === 'yarn';
 
   const originalDirectory = process.cwd();
 
@@ -37,6 +48,8 @@ export const createApp = async ({
   process.chdir(root);
 
   const packageJsonPath = path.join(root, 'package.json');
+
+  const hasPackageJson = fs.existsSync(packageJsonPath);
 
   await installTemplate({
     appName,
@@ -60,4 +73,27 @@ export const createApp = async ({
   }
 
   console.log(`${chalk.green('Success!')} Created ${appName} at ${appPath}`);
+
+  if (hasPackageJson) {
+    console.log('Inside that directory, you can run several commands:');
+    console.log();
+    console.log(chalk.cyan(`  ${packageManager} ${useYarn ? '' : 'run '}dev`));
+    console.log('    Starts the development server.');
+    console.log();
+    console.log(
+      chalk.cyan(`  ${packageManager} ${useYarn ? '' : 'run '}build`),
+    );
+    console.log('    Builds the app for production.');
+    console.log();
+    console.log(chalk.cyan(`  ${packageManager} start`));
+    console.log('    Runs the built app in production mode.');
+    console.log();
+    console.log('We suggest that you begin by typing:');
+    console.log();
+    console.log(chalk.cyan('  cd'), cdpath);
+    console.log(
+      `  ${chalk.cyan(`${packageManager} ${useYarn ? '' : 'run '}dev`)}`,
+    );
+    console.log();
+  }
 };
