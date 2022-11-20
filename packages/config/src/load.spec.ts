a31028fbe4c03098582ea046d72603193941c365
kakeng <kakengloh@gmail.com>
2022-11-20 23:02:11 +0800

    Support variables parsing and tests

--- packages/config/src/load.spec.ts
+++ packages/config/src/load.spec.ts
@@ -1,21 +1,43 @@
-import { describe, expect, test } from 'vitest';
+import { afterEach, describe, expect, test } from 'vitest';
 import path from 'path';
-import { load } from './load';
+import { getConfigFilePath, load } from './load';
 
 describe('load()', () => {
-  test('should load basic config from file', () => {
+  const env = { ...process.env };
+
+  afterEach(() => {
+    process.env = env;
+  });
+
+  test('should load config from file', () => {
     const config = load({
-      path: path.join(__dirname, '__mocks__', 'basic.json'),
+      path: path.join(__dirname, '__mocks__', 'config', 'basic.json'),
     });
     expect(config).to.deep.equals({ port: 3000 });
   });
 
-  test('should load config and parse secrets from file ', () => {
+  test('should load config from file and parse specific variables', () => {
+    const config = load({
+      path: path.join(__dirname, '__mocks__', 'config', 'variables.json'),
+      variables: {
+        AWS_ACCESS_KEY_ID: '123',
+        AWS_SECRET_ACCESS_KEY: '456',
+      },
+    });
+    expect(config).to.deep.equals({
+      aws: {
+        accessKeyId: '123',
+        secretAccessKey: '456',
+      },
+    });
+  });
+
+  test('should load config from file and parse variables from process.env', () => {
     process.env.JWT_SECRET = 'jwt-secret';
     process.env.DB_PASS = 'db-pass';
 
     const config = load({
-      path: path.join(__dirname, '__mocks__', 'env.json'),
+      path: path.join(__dirname, '__mocks__', 'config', 'env.json'),
     });
 
     expect(config).to.deep.equals({
@@ -30,5 +52,34 @@ describe('load()', () => {
 
     delete process.env.JWT_SECRET;
     delete process.env.DB_PASS;
+  });
+
+  test('should load config from file and parse variables from .env file', () => {
+    const config = load({
+      path: path.join(__dirname, '__mocks__', 'config', 'dotenv.json'),
+      dotenv: true,
+    });
+
+    expect(config).to.deep.equals({
+      aws: {
+        accessKeyId: '123',
+        secretAccessKey: '456',
+      },
+    });
+  });
+});
+
+describe('getConfigFilePath()', () => {
+  test('should return correct config file path with current environment (test)', () => {
+    expect(getConfigFilePath()).toEqual(
+      path.join(__dirname, '..', 'config', 'test.json'),
+    );
+  });
+
+  test('should return correct config file path with default environment (development)', () => {
+    process.env.NODE_ENV = undefined;
+    expect(getConfigFilePath()).toEqual(
+      path.join(__dirname, '..', 'config', 'development.json'),
+    );
   });
 });
--- packages/config/src/load.ts
+++ packages/config/src/load.ts
@@ -1,19 +1,45 @@
-import { readFileSync } from 'fs';
+import { existsSync, readFileSync } from 'fs';
+import { join } from 'path';
 
 export interface LoadOptions {
   path?: string;
-  secrets?: object;
+  variables?: object;
+  dotenv?: boolean;
+}
+
+export function getConfigFilePath(
+  env: string = process.env.NODE_ENV || 'development',
+): string {
+  return `${process.cwd()}/config/${env}.json`;
 }
 
 export function load<T extends object>({
-  path = '',
-  secrets = process.env,
+  path = getConfigFilePath(),
+  variables = {},
+  dotenv = true,
 }: LoadOptions = {}): T {
+  if (!existsSync(path)) {
+    throw new Error(`Config file not found at path: '${path}'`);
+  }
+
+  // Load .env file
+  if (dotenv) {
+    require('dotenv').config({
+      path: join(path, '..', '..', '.env'),
+    });
+    variables = {
+      ...process.env,
+      ...variables,
+    };
+  }
+
+  // Read config file
   let content = readFileSync(path, 'utf-8');
 
-  Object.entries(secrets).forEach(([key, value]) => {
+  // Parse variables
+  Object.entries(variables).forEach(([key, value]) => {
     const re = new RegExp('{{(?:\\s+)?(' + key + ')(?:\\s+)?}}', 'g');
-    content = content.replaceAll(re, value);
+    content = content.replace(re, value);
   });
 
   return JSON.parse(content) as T;
