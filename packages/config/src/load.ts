92db8479b5bac1e403b63c058574602364166e00
kakeng <kakengloh@gmail.com>
2022-11-30 11:50:14 +0800

    Use path join to support cross platform

--- packages/config/src/load.ts
+++ packages/config/src/load.ts
@@ -3,6 +3,7 @@ import { AnyZodObject } from 'zod';
 import yaml from 'yaml';
 import dotenv from 'dotenv';
 import * as eta from 'eta';
+import path from 'path';
 
 export interface LoadOptions {
   path?: string | null;
@@ -13,7 +14,7 @@ export interface LoadOptions {
 export function getConfigFilePath(
   env: string = process.env.NODE_ENV || 'development',
 ): string | null {
-  const file = `${process.cwd()}/config/${env}`;
+  const file = path.join(process.cwd(), 'config', env);
   const extensions = ['.yml', '.yaml'];
 
   for (const extension of extensions) {
